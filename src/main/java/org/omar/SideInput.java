265593e1ede7977541f8992bb626dd2fc9d362f8
omarismail94 <omarm.ismail@mail.utoronto.ca>
2020-04-10 22:05:28 -0400

    Added Side Input

---
+++ src/main/java/org/omar/SideInput.java
@@ -0,0 +1,70 @@
+package org.omar;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.Max;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionView;
+
+public class SideInput {
+    public interface MyOptions extends PipelineOptions{
+        @Validation.Required
+        @Default.String("bible.txt")
+        ValueProvider<String> getInputFile();
+        void setInputFile(ValueProvider<String> value);
+
+        @Validation.Required
+        @Default.String("results/James")
+        ValueProvider<String> getOutput();
+        void setOutput(ValueProvider<String> value);
+    }
+
+    public static void main(String[] args) {
+        MyOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(MyOptions.class);
+        runSider(options);
+    }
+
+    private static void runSider(MyOptions options) {
+        Pipeline p = Pipeline.create(options);
+
+        PCollection<String> wordLines =  p.apply(TextIO.read().from(options.getInputFile()));
+
+        PCollection<Integer> wordLengths = wordLines.apply(ParDo.of(new DoFn<String, Integer>() {
+            @ProcessElement
+            public void processElement(@Element String word, OutputReceiver<Integer> lengthy){
+                lengthy.output(word.split("\\s+").length);
+            }
+
+        }));
+
+        final PCollectionView<Integer> maxWordLength = wordLengths.apply(Combine.globally(Max.ofIntegers()).asSingletonView());
+
+        PCollection<String> wordsBelowCutOff =
+                wordLines.apply(ParDo
+                        .of(new DoFn<String, String>() {
+                            @ProcessElement
+                            public void processElement(@Element String word, OutputReceiver<String> out, ProcessContext c) {
+                                int lengthCutOff = c.sideInput(maxWordLength);
+                                if (word.split("\\s+").length == lengthCutOff) {
+                                    out.output(word);
+                                }
+                            }
+                        }).withSideInputs(maxWordLength)
+                );
+
+        wordsBelowCutOff.apply(TextIO.write().to(options.getOutput()));
+        p.run().waitUntilFinish();
+    }
+
+    public static class ConvertToString extends  DoFn<Integer,String> {
+        @ProcessElement
+        public void processElement(@Element Integer inNum, OutputReceiver<String> outString){
+            outString.output(inNum.toString());
+        }
+
+    }
+}
